4082e2a2c289f354166039cc1c79209fa229a33e
garam <garam-park@naver.com>
2022-08-19 15:47:55 +0900

    page3

--- tests/page1.test.ts
+++ tests/page1.test.ts
@@ -1,15 +1,15 @@
 // Regular expressions are case sensitive. Therefore Case 1 will find the specified text, but Case 2 will not.
 // 정규표현식은 대소문자를 구분합니다. 그러므로 케이스1는 문자열을 찾을수 있지만 케이스2는 찾지 못합니다.
 
+const source = "Hello, world!";
+
 test("case1", () => {
-  const source = "Hello, world!";
-  const reg_exp = /Hello/;
+  const reg_exp = /Hello/g;
 
   expect(reg_exp.test(source)).toBe(true);
 });
 
 test("case2", () => {
-  const source = "Hello, world!";
   const reg_exp = /hello/;
 
   expect(reg_exp.test(source)).toBe(false);
